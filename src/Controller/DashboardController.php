6e71ba3a6294e5afecffba7836c48eeb98d602b3
Francois <francois.coche@outlook.it>
2022-10-25 12:14:44 +0200

    Code of personnal dashboard, controller + template

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Bookmark;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\Persistence\ManagerRegistry;
 class DashboardController extends AbstractController
 {
+    /**
+     * Return user fav currency to the dashboard page
+     *
+     * @param ManagerRegistry $doctrine
+     * @return Response
+     */
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $user = $this->getUser();
+
+        // if there is no connected user, stop the process
+        if(!$user) return $this->json([
+            'message' => 'Unauthorized'
+        ], 403);
+
+        // get all fav user coins from database
+        $favCoins = $doctrine->getRepository(Bookmark::class)->findBy(['user' => $user]);
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'favCoins' => $favCoins,
         ]);
     }
 }
